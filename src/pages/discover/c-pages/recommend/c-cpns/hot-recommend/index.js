46fcbb1eb7ef329cf6031c687581be92ebf32c61
叶继伟 <729721503@qq.com>
2021-12-12 20:51:09 +0800

    feat:  音乐推荐页面 新碟上架 排行榜完成

---
+++ src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
@@ -0,0 +1,37 @@
+import React, { memo,useEffect } from 'react'
+import { useSelector, useDispatch, shallowEqual } from 'react-redux'
+import ThemeHeaderRCM from '@/components/theme-header-rcm'
+import SongsCover from '@/components/songs-cover';
+import {
+  HotRecommendWrapper
+} from './style';
+
+import { getHotRecommendAction } from '../../store/actionCreators';
+import { HOT_RECOMMEND_LIMIT } from '@/common/contants';
+export default memo(function HotRecommend() {
+  // state
+
+  // redux hooks
+  const { hotRecommends } = useSelector(state => ({
+    hotRecommends: state.getIn(["recommend", "hotRecommends"])
+  }), shallowEqual);
+  const dispatch = useDispatch();
+
+  // other hooks
+  useEffect(() => {
+    dispatch(getHotRecommendAction(HOT_RECOMMEND_LIMIT));
+  }, [dispatch]);
+
+  return (
+    <HotRecommendWrapper>
+      <ThemeHeaderRCM title="热门推荐" keywords={["华语", "流行", "民谣", "摇滚", "电子"]} />
+      <div className="recommend-list">
+        {
+          hotRecommends.map((item, index) => {
+            return <SongsCover key={item.id} info={item}/>
+          })
+        }
+      </div>
+    </HotRecommendWrapper>
+  )
+})
--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -1,25 +1,38 @@
 import React, { memo, useEffect } from 'react'
-// import { connect } from 'react-redux'
 import { useDispatch, useSelector, shallowEqual } from 'react-redux'
 import { getTopBannerAction } from './store/actionCreators'
 
+import TopBanner from './c-cpns/top-banner'
+import HotRecommend from './c-cpns/hot-recommend'
+import NewAlbum from './c-cpns/new-album'
+import RecommendRanking from './c-cpns/recommend-ranking'
+import UserLogin  from './c-cpns/user-login'
+import SettleSinger from './c-cpns/settle-singer'
+import HotAnchor from './c-cpns/hot-anchor'
+import { 
+  RecommendWrapper,
+  Content,
+  RecommendLeft,
+  RecommendRight 
+} 
+from './style'
 function Recommend(props) {
-  
-  const recommend = useSelector(state => ({
-      topBanners: state.get('recommend').get('topBanners')
-  }), shallowEqual)
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch(getTopBannerAction())
-    return () => {
-    }
-  }, [dispatch])
-
   return (
-    <div>
-      recommend: {recommend.topBanners.length}
-    </div>
+    <RecommendWrapper>
+      <TopBanner/>
+      <Content className="wrap-v2">
+        <RecommendLeft>
+          <HotRecommend/>
+          <NewAlbum/>
+          <RecommendRanking/>
+        </RecommendLeft>
+        <RecommendRight>
+          <UserLogin/>
+          <SettleSinger/>
+          <HotAnchor/>
+        </RecommendRight>
+      </Content>
+    </RecommendWrapper>
   )
 }
 
